03d7d5f870a78f835289d7a9e02b7ded95a29c51
lgeng12 <larry.geng12@gmail.com>
2019-01-07 23:11:25 -0500

    half fixes to Crusader

--- initial/Crusader.java
+++ initial/Crusader.java
@@ -1,20 +1,19 @@
-import java.util.*;
-
 package bc19;
+
+import java.util.*;
+final int ATTACK_RANGE = 6;
 
 public class Crusader extends MyRobot {
 
-    public static Action turn(BCAbstractRobot r) {
+    public static Action takeTurn() {
         TreeMap<Integer, Integer> enemyBotDistance = new TreeMap<Integer, Integer>();
         TreeMap<Integer, Integer> friendlyBotDistance = new TreeMap<Integer, Integer>();
-
-        boolean[][] currentMap = r.me.getPassableMap();
 
         if(r.me.turn == 1)
             r.log("i was made" + r.me.id);
 
         // look at nearby bots
-        for(Robot other: r.me.getVisibleRobots()){
+        for(Robot other: r.getVisibleRobots()){
             if(other.team == r.me.team)
                 friendlyBotDistance.put(getSquaredDistance(r.me, other), other.id);
             else
@@ -25,8 +24,8 @@ public class Crusader extends MyRobot {
         if(enemyBotDistance.size() <= 0){
             int dx = (int)(Math.random() * 2 * r.me.unit.SPEED - r.me.unit.SPEED + 1);
             int dy = (int)(Math.random() * 2 * r.me.unit.SPEED - r.me.unit.SPEED + 1);
-            if(isEmpty(r, r.me.x + dx, r.me.y + dy))
-                return r.me.move(dx, dy);
+            if(isEmpty(r.me.x + dx, r.me.y + dy))
+                return r.move(dx, dy);
             else
                 return null;
         }
@@ -35,10 +34,10 @@ public class Crusader extends MyRobot {
 
         // attack enemy
         if(enemyBotDistance.firstKey() < r.me.unit.ATTACK_RADIUS)
-            return attackRobot(r.me, closestEnemy);
+            return attackRobot(closestEnemy);
 
         // get closer to enemy
-        return r.me.move(dx, dy);
+        return moveToRobot(closestEnemy);
 
     }
 
@@ -48,24 +47,24 @@ public class Crusader extends MyRobot {
         return dx*dx + dy*dy;
     }
 
-    public static Action attackRobot(Robot me, Robot other){
-        return me.attack(other.x - me.x, other.y - me.y);
+    public static Action attackRobot(Robot other){
+        return r.attack(other.x - r.me.x, other.y - r.me.y);
     }
 
-    public static Action moveToRobot(BCAbstractRobot r, Robot me, Robot other){
-        int dx = other.x - me.x;
-        int dy = other.y - me.y;
+    public static Action moveToRobot(Robot other){
+        int dx = other.x - r.me.x;
+        int dy = other.y - r.me.y;
         // idk how to do this
         if(dx != 0)
             dx = 1;
         if(dy != 0)
             dy = 1;
-        if(isEmpty(r, me.x + dx, me.y + dy))
-            return me.move(dx, dy);
-        return null
+        if(isEmpty(r.me.x + dx, r.me.y + dy))
+            return r.move(dx, dy);
+        return null;
     }
 
-    static boolean isEmpty(BCAbstractBot r, int x, int y) {
+    static boolean isEmpty(int x, int y) {
         boolean[][] passableMap = r.getPassableMap();
         int[][] visibleRobotMap = r.getVisibleRobotMap();
         return passableMap[y][x] && visibleRobotMap[y][x] == 0;
