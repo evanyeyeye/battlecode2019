89b93538f4a34d5594c8c7784a67e59eba0ea8fd
lgeng12 <larry.geng12@gmail.com>
2019-01-08 22:58:15 -0500

    crusader fixes

--- initial/Crusader.java
+++ initial/Crusader.java
@@ -5,6 +5,7 @@ import java.util.*;
 public class Crusader extends MyRobot {
 
     static Action takeTurn() {
+        int sqrtSpeed = (int)(Math.sqrt(0.5 * r.SPECS.UNITS[r.SPECS.CRUSADER].SPEED));  // some sort of scaling value
         TreeMap<Integer, Integer> enemyBotDistance = new TreeMap<Integer, Integer>();
         TreeMap<Integer, Integer> friendlyBotDistance = new TreeMap<Integer, Integer>();
 
@@ -12,27 +13,38 @@ public class Crusader extends MyRobot {
             r.log("I am a Crusader");
 
         // look at nearby bots
+        // TEMP?
+        Robot closestEnemy = null;
+        int minEnemyDistance = 100;
+
         for(Robot other: r.getVisibleRobots()){
             if(other.team == r.me.team)
                 friendlyBotDistance.put(getSquaredDistance(r.me, other), other.id);
-            else
+            else{
                 enemyBotDistance.put(getSquaredDistance(r.me, other), other.id);
+                if(getSquaredDistance(r.me, other) < minEnemyDistance){
+                    minEnemyDistance = getSquaredDistance(r.me, other);
+                    closestEnemy = other;
+                }
+            }
         }
 
         // move if no enemies
-        if(enemyBotDistance.size() <= 0){
-            int dx = (int)(Math.random() * 2 * r.SPECS.UNITS[r.SPECS.CRUSADER].SPEED - r.SPECS.UNITS[r.SPECS.CRUSADER].SPEED + 1);
-            int dy = (int)(Math.random() * 2 * r.SPECS.UNITS[r.SPECS.CRUSADER].SPEED - r.SPECS.UNITS[r.SPECS.CRUSADER].SPEED + 1);  // TODO: Replace constants
+        if(closestEnemy == null){
+            int dx = (int)(Math.random() * (2 * sqrtSpeed + 1) - sqrtSpeed); 
+            int dy = (int)(Math.random() * (2 * sqrtSpeed + 1) - sqrtSpeed);
             if(isEmpty(r.me.x + dx, r.me.y + dy))
                 return r.move(dx, dy);
             else
                 return null;
         }
 
-        Robot closestEnemy = r.getRobot(enemyBotDistance.firstEntry().getValue());
+        // Robot closestEnemy = r.getRobot(enemyBotDistance.get(enemyBotDistance.firstKey()));  // why is this bugged
 
         // attack enemy
-        if(enemyBotDistance.firstKey() < r.SPECS.UNITS[r.SPECS.CRUSADER].ATTACK_RADIUS[1])  // TODO: Replace constants
+        // if(enemyBotDistance.firstKey() < r.SPECS.UNITS[r.SPECS.CRUSADER].ATTACK_RADIUS[1])
+        //     return attackRobot(closestEnemy);
+        if(minEnemyDistance < r.SPECS.UNITS[r.SPECS.CRUSADER].ATTACK_RADIUS[1])
             return attackRobot(closestEnemy);
 
         // get closer to enemy
@@ -53,11 +65,15 @@ public class Crusader extends MyRobot {
     public static Action moveToRobot(Robot other){
         int dx = other.x - r.me.x;
         int dy = other.y - r.me.y;
-        // idk how to do this
-        if(dx != 0)
+        // idk how to do this don't look too closely
+        if(dx > 0)
             dx = 1;
-        if(dy != 0)
+        if(dy > 0)
             dy = 1;
+        if(dx < 0)
+            dx = -1;
+        if(dy < 0)
+            dy = -1;
         if(isEmpty(r.me.x + dx, r.me.y + dy))
             return r.move(dx, dy);
         return null;
