bdf6bbbe5c24ac1ce929d846958c49c7bc158bc0
Sean Yang <--replace-all>
2019-01-10 00:36:41 -0500

    updated pilgrim not fully working

--- initial_js/castle.js
+++ initial_js/castle.js
@@ -24,13 +24,13 @@ export function castleTurn(r) {
     if (r.me.turn == 1) {
         r.log("I am a Castle")
     }
-    if (r.karbonite > SPECS.UNITS[SPECS.CRUSADER].CONSTRUCTION_KARBONITE && r.fuel > SPECS.UNITS[SPECS.CRUSADER].CONSTRUCTION_FUEL && crusaderCounter * 300 < r.me.turn) {
+    if (r.karbonite > SPECS.UNITS[SPECS.PILGRIM].CONSTRUCTION_KARBONITE && r.fuel > SPECS.UNITS[SPECS.CRUSADER].CONSTRUCTION_FUEL && crusaderCounter * 300 < r.me.turn) {
         var buildDirection = findBuildDirection(r, r.me.x, r.me.y)
         if (buildDirection != null) {
             r.log("Built Crusader")
-            return r.buildUnit(SPECS.CRUSADER, buildDirection[1], buildDirection[0])
+            return r.buildUnit(SPECS.PILGRIM, buildDirection[1], buildDirection[0])
         }
-        crusaderCounter++
+        pilgrimCounter++
     }
     return
 }
--- initial_js/pilgrim.js
+++ initial_js/pilgrim.js
@@ -1,5 +1,43 @@
 import {SPECS} from 'battlecode'
-
+var targetLoc=[-1,-1]; //position to go if no safe mine seen. Go according to caslte's direction
+var unsafeLoc=[]; //from castle to see 
+var prioResoruce=-1; //0 is karbonite 1 is fuel
+var karboniteMine=[] //list containing all of the karbonite only fill in if it's empty
+var fuelMine=[] //list containing all of the fuel only fill in if it's empty
 export function pilgrimTurn(r) {
     return
+}
+//find the best move
+function findMove(r){
+
+	var target=null;
+	var targetMine=ClosestSafeMine()
+	if (targetMine==null){
+		target=targetLoc
+	}	
+	var pf = r.pm.getPathField([targetLoc]) //going to the location
+    if (r.fuel > SPECS.UNITS[SPECS.PILGRIM].FUEL_PER_MOVE) {
+        var test = pf.getDirectionAtPoint(r.me.x, r.me.y)
+        return r.move(test[1], test[0])
+    }
+
+
+}
+//return a set of all the places that have the potential to get attacked. 
+//Check currentt location and mines to see
+function checkDanger(r){
+
+
+
+}
+//check where the closest safe mine is 
+function ClosestSafeMine(r){
+	var dangerSet=checkDanger(r);
+	for 
+
+}
+//just straight up distance
+function filter(mine,r){
+	myx=r.me.x;
+	myy=r.me.y;
 }
