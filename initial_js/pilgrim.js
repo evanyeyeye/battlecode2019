691d2767650480bc3282381bdf9e5d9eb8c6a116
lgeng12 <larry.geng12@gmail.com>
2019-01-11 15:54:50 -0500

    fix some of pilgrim

--- initial_js/pilgrim.js
+++ initial_js/pilgrim.js
@@ -38,10 +38,10 @@ export function pilgrimTurn(r) {
 
     // look around
     for (let otherRobot of r.getVisibleRobots()) {
-        let distance = getSquaredDistance(r.me.x, r.me.y, otherRobot.x, otherRobot.y)
+        let distance = getManhattanDistance(r.me.x, r.me.y, otherRobot.x, otherRobot.y)
         if (otherRobot.team == r.me.team) {
             // set closest friendly castle as base
-            if (baseCastleLocation == null  || getSquaredDistance(r.me.x, r.me.y, baseCastleLocation[0], baseCastleLocation[1]) < distance) {
+            if (baseCastleLocation == null  || getManhattanDistance(r.me.x, r.me.y, baseCastleLocation[0], baseCastleLocation[1]) < distance) {
                 baseCastleLocation = [otherRobot.x, otherRobot.y]
             }
             friendlyRobots[otherRobot.id] = distance
@@ -53,7 +53,7 @@ export function pilgrimTurn(r) {
 
     // return to castle if full
     if (r.me.karbonite == SPECS.UNITS[SPECS.PILGRIM].KARBONITE_CAPACITY || r.me.fuel == SPECS.UNITS[SPECS.PILGRIM].FUEL_CAPACITY) {
-        let pf = r.pm.getPathField(baseCastleLocation)
+        let pf = r.pm.getPathField(baseCastleLocation.reverse())
         if (r.fuel > SPECS.UNITS[SPECS.PILGRIM].FUEL_PER_MOVE) {
             let test = pf.getDirectionAtPoint(r.me.x, r.me.y)  // uses pathfinding
             return r.move(test[1], test[0])
@@ -65,8 +65,8 @@ export function pilgrimTurn(r) {
 	let targetMine = closestSafeMine(r)
 
     // check if on top of mine
-    if (targetMine != null && getSquaredDistance(r.me.x, r.me.y, targetMine[1], targetMine[0]) == 0) {
-        r.log("i'm actually trying to mine")
+    if (targetMine != null && getManhattanDistance(r.me.x, r.me.y, targetMine[0], targetMine[1]) == 0) {
+        r.log("i'm actually trying to mine at " + targetMine[0] + ", " + targetMine[1])
         return r.mine()
     }
 
@@ -76,7 +76,7 @@ export function pilgrimTurn(r) {
 	}
 
     // path to location
-	let pf = r.pm.getPathField(targetMine)
+	let pf = r.pm.getPathField(targetMine.reverse())
     if (r.fuel > SPECS.UNITS[SPECS.PILGRIM].FUEL_PER_MOVE) {
         r.log("I want to move to " + targetMine)
         let test = pf.getDirectionAtPoint(r.me.x, r.me.y)  // uses pathfinding
@@ -86,39 +86,44 @@ export function pilgrimTurn(r) {
 }
 
 function isEmpty(r, x, y) {
-    var passableMap = r.map;
-    var visibleRobotMap = r.getVisibleRobotMap();
+    let passableMap = r.map;
+    let visibleRobotMap = r.getVisibleRobotMap();
     return passableMap[y][x] && visibleRobotMap[y][x] == 0;
 }
 
+// update mine locatoins based on distance from 
 function updateMines(r) {
-    for (let i = 0; i<r.karbonite_map.length; i++) {
-        for (let j = 0; j<r.karbonite_map[0].length; j++) {
-            if (r.karbonite_map[i][j]) {
-                karboniteMines[[i, j]] = getSquaredDistance(j, i, r.me.x, r.me.y)  // confirm this ordering, idk
+    for (let j = 0; j<r.karbonite_map.length; j++) {
+        for (let i = 0; i<r.karbonite_map[0].length; i++) {
+            if (r.karbonite_map[j][i]) {
+                karboniteMines[[i, j]] = getManhattanDistance(i, j, baseCastleLocation[0], baseCastleLocation[1])  // confirm this ordering, idk
             }
-            if (r.fuel_map[i][j]) {
-                fuelMines[[i, j]] = getSquaredDistance(j, i, r.me.x, r.me.y)
+            if (r.fuel_map[j][i]) {
+                fuelMines[[i, j]] = getManhattanDistance(i, j, baseCastleLocation[0], baseCastleLocation[1])
             }
         }
     }
 }
 
 function getSquaredDistance(x1, y1, x2, y2) {
-    return (x2 - x1)**2 + (y1 - y2)**2
+    return (x2 - x1)**2 + (y2 - y1)**2
+}
+
+function getManhattanDistance(x1, y1, x2, y2) {
+return Math.abs(x2 - x1) + Math.abs(y2 - y1)
 }
 
 // check all mines that are dangerous according to units in vision
 // later check if you cna safely mine
 function checkDanger(r) {
-    var merged = new Set([karboniteMines, fuelMines])
+    let merged = new Set([karboniteMines, fuelMines])
    
-    var visible = r.getVisibleRobots()
-    for (var curMine in merged) {
+    let visible = r.getVisibleRobots()
+    for (let curMine in merged) {
         // || unsafeLoc.has(curMine) not sure if you check unsafe loc or not
         // only do it if something can possibly be attacked by thigns in vision
         if (getSquaredDistance(r.x,r.y,curMine[0],curMine[1])**0.5<18){
-            for (var robot in visible){
+            for (let robot in visible){
                 if (inRange(robot,curMine)){
                     unsafeLoc.add(curMine);
 
@@ -132,7 +137,7 @@ function checkDanger(r) {
 
 // check where the closest safe mine is 
 function closestSafeMine(r) {
-	// let dangerSet = checkDanger(r)
+	let dangerSet = checkDanger(r)
     let mines = karboniteMines
     if (priorityResource == FUEL)
         mines = fuelMines
@@ -151,11 +156,11 @@ function closestSafeMine(r) {
 function findBuildLocation(r) {
     // for building churches
 }
-function inRange(r,l){
+
+function inRange(r,l) {
     //not sure if location is x or y
    if (getSquaredDistance(r.x,r.y,l[0],l[1])<SPECS.UNITS[SPECS.r.unit].ATTACK_RADIUS[1]){
-
-    return true;    
+        return true;    
    }
    return false;
 
