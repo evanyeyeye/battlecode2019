7859e6467f51e8c47e6a27afa4232c70d56b99fc
lgeng12 <larry.geng12@gmail.com>
2019-01-12 00:17:31 -0500

    pilgrim cleanup

--- initial_js/castle.js
+++ initial_js/castle.js
@@ -17,15 +17,30 @@ function findBuildDirection(r, x, y) {
     return null
 }
 
+function iDMines(r) {  // deterministically label mines
+    let counter = 0
+    for (let j = 0; j < r.karbonite_map.length; j++) {
+        for (let i = 0; i < r.karbonite_map[0].length; i++) {
+            if (r.karbonite_map[j][i] || r.fuel_map[j][i]){
+                r.log("Castle: Mine at " + [i, j] + " is " + counter)
+                mineID[[i, j]] = counter
+                counter++
+            }
+        }
+    }
+}
+
+var mineID = {}
 var pilgrimCounter = 0
 var crusaderCounter = 0
 
 export function castleTurn(r) {
     if (r.me.turn == 1) {
         r.log("I am a Castle")
+        iDMines(r)
     }
     // build pilgrims
-    if (pilgrimCounter<3&& r.karbonite > SPECS.UNITS[SPECS.PILGRIM].CONSTRUCTION_KARBONITE && r.fuel > SPECS.UNITS[SPECS.PILGRIM].CONSTRUCTION_FUEL && pilgrimCounter * 300 < r.me.turn) {
+    if (pilgrimCounter < 3 && r.karbonite > SPECS.UNITS[SPECS.PILGRIM].CONSTRUCTION_KARBONITE && r.fuel > SPECS.UNITS[SPECS.PILGRIM].CONSTRUCTION_FUEL && pilgrimCounter * 300 < r.me.turn) {
         var buildDirection = findBuildDirection(r, r.me.x, r.me.y)
         if (buildDirection != null) {
             r.log("Built Pilgrim")
