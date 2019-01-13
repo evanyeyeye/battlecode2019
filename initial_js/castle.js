631db3074517bcc577aea548a1b320d5e0216ac8
Sean Yang <--replace-all>
2019-01-13 14:33:08 -0500

    this is a broken prophet code

--- initial_js/castle.js
+++ initial_js/castle.js
@@ -72,7 +72,7 @@ var fMineID = {}
 var karboniteMines = {}  // maps mine id to distance from this castle
 var fuelMines = {}
 
-var totalMines = 0 // total mine is number of total mines
+var totalMines = 0 // total mine is number of total 
 var numKMines = 0  // maybe not all necessary, check dictionary lengths instead?
 var numFMines = 0
 var numMines =0; //numMines is number of closem ines
@@ -112,7 +112,7 @@ export function castleTurn(r) {
 
     // r.log("The round is: " + r.me.turn)
     // build pilgrims
-    if (pilgrimCounter < numMines+2 && r.karbonite > SPECS.UNITS[SPECS.PILGRIM].CONSTRUCTION_KARBONITE && r.fuel > SPECS.UNITS[SPECS.PILGRIM].CONSTRUCTION_FUEL) {
+    if (pilgrimCounter < 1 && r.karbonite > SPECS.UNITS[SPECS.PILGRIM].CONSTRUCTION_KARBONITE && r.fuel > SPECS.UNITS[SPECS.PILGRIM].CONSTRUCTION_FUEL) {
         var buildDirection = findBuildDirection(r, r.me.x, r.me.y)
         if (buildDirection != null) {
             r.log("Built Pilgrim")
@@ -120,7 +120,7 @@ export function castleTurn(r) {
             return r.buildUnit(SPECS.PILGRIM, buildDirection[0], buildDirection[1])
         }
     }
-/*
+
     if (r.karbonite > SPECS.UNITS[SPECS.PROPHET].CONSTRUCTION_KARBONITE && r.fuel > SPECS.UNITS[SPECS.PROPHET].CONSTRUCTION_FUEL) {
         var buildDirection = findBuildDirection(r, r.me.x, r.me.y)
         if (buildDirection != null) {
@@ -129,7 +129,7 @@ export function castleTurn(r) {
             return r.buildUnit(SPECS.PROPHET, buildDirection[0], buildDirection[1])
         }
     }
-    */
+   
 
     // // build crusaders
     // if (r.karbonite > SPECS.UNITS[SPECS.CRUSADER].CONSTRUCTION_KARBONITE && r.fuel > SPECS.UNITS[SPECS.CRUSADER].CONSTRUCTION_FUEL && crusaderCounter * 300 < r.me.turn) {
