e3707ef5bceb4e9755d036441a4813520adbb714
Sean Yang <--replace-all>
2019-01-20 10:36:34 -0500

    no merge pls

--- seeding/comms.js
+++ seeding/comms.js
@@ -36,6 +36,34 @@ export default {
         return encoded
     },
 
+
+    // encode message for attacking cordinate in x y
+    
+    encodeAttack: function (x, y) {
+        let encoded_mine=mineID.toString(2);
+        let encoded_mine2=mineID2.toString(2);
+        let bitsToGive=Math.ceil(Math.log2(totalMines)) // how many bits to give
+        let message=""   
+        //fill up the empty spots 
+        for (let i=0; i<bitsToGive- encoded_mine.length ;i++){
+            message+="0"
+        }   
+        message+=encoded_mine    
+        message+=action
+        //fill up the empty spots 
+        for (let i=0; i<bitsToGive- encoded_mine2.length ;i++){
+            message+="0"
+        }  
+        message+=encoded_mine2
+        let msglen=message.length
+        //fill up the empty spots
+        for (let i=0; i<signallen -msglen ;i++){
+            message+="0"
+        }    
+        let encoded = parseInt(message, 2);
+        return encoded
+    },
+
     // used to decode mine the 
     // returns a list with mine1,mine2,action in order
     decodeSignal: function (message, totalMines, signallen) {
