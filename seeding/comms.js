77e9fcef95cfb322a3a8ec4390c70beac6800d39
Sean Yang <--replace-all>
2019-01-20 13:41:46 -0500

    added killcastle signal

--- seeding/comms.js
+++ seeding/comms.js
@@ -9,6 +9,7 @@ export default {
 
     BUILDING_CHURCH: "999",
     ALL_IN: "1000",
+    KILLED: "1001",
 
     // encode message for signaling
     // action is a number
@@ -68,6 +69,39 @@ export default {
     return encoded
     },
 
+    //killed castle
+    encodeCastleKill: function (x, y) {
+        let encoded_mine=x.toString(2);
+        let encoded_mine2=y.toString(2);
+        let totalMines=64 // decide how many bits to give to mines
+        let bitsToGive=Math.ceil(Math.log2(totalMines)) // how many bits to give
+        let message=""   
+        //fill up the empty spots 
+        for (let i=0; i<bitsToGive- encoded_mine.length ;i++){
+            message+="0"
+        }   
+        message+=encoded_mine    
+        message+="00"
+        //fill up the empty spots 
+        for (let i=0; i<bitsToGive- encoded_mine2.length ;i++){
+            message+="0"
+        }  
+        message+=encoded_mine2
+        let msglen=message.length
+        //fill up the empty spots
+        for (let i=0; i<signallen-1 -msglen ;i++){
+            message+="1"
+    }    
+
+    message+="0"
+    //console.log(message)
+    let encoded = parseInt(message, 2);
+  
+    return encoded
+    },
+
+
+
     // used to decode mine the 
     // returns a list with mine1,mine2,action in order
     decodeSignal: function (message, totalMines, signallen) {
@@ -87,6 +121,18 @@ export default {
         let mineID2=parseInt(binary.substring(bitsToGive+2,bitsToGive+2+bitsToGive),2); 
         //this is really giving (x,y,attack)  
         return [mineID,mineID2,"1000"] 
+    }
+    if (binary[binary.length-1] =="0" && binary[binary.length-2] == "1")
+    {
+           
+        let numtaken=64 // decide how many bits to give to mines
+        let bitsToGive=Math.ceil(Math.log2(numtaken)) // how many bits to give
+        let firstMine=binary.substring(0,bitsToGive)    
+        let action=binary.substring(bitsToGive,bitsToGive+2)  
+        let mineID = parseInt(firstMine, 2);
+        let mineID2=parseInt(binary.substring(bitsToGive+2,bitsToGive+2+bitsToGive),2); 
+        //this is really giving (x,y,attack)  
+        return [mineID,mineID2,"1001"] 
     }
     else{
         let bitsToGive=Math.ceil(Math.log2(totalMines)) // how many bits to give
